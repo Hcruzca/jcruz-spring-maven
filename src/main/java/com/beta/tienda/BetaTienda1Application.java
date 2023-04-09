5e454c54edbfc4504c89ffa38cc3820dc4fa1079
Hipolito <hipolitojcca@gmail.com>
2023-04-09 14:09:12 -0600

    reconfiguracion

---
+++ src/main/java/com/beta/tienda/BetaTienda1Application.java
@@ -0,0 +1,13 @@
+package com.beta.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaTienda1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaTienda1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/beta/tienda/BetaTienda1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beta.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaTienda1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
